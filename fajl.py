4690b51d3e989e44384bede13380f8b8484daaa4
jaksamarko-sys <jaksa.marko@diak.szbi-pg.hu>
2025-03-12 13:35:14 +0100

    2nd task

--- fajl.py
+++ fajl.py
@@ -1,1 +1,24 @@
-with open ('')
+'''1. Feladat
+A mellékelt fájl néhány ismert programozási nyelv adatát tartalmazza. Olvasd be a fájl tartalmát, és másold át azt egy fájlba úgy, hogy abba már csak a nyelv és az évszám kerüljön!
+
+(Fájl letöltése: kattints a "Forrásfájl" feliratú gombra az egér jobb gombjával, és a felugró menüből válaszd a "Link mentése másként..." opciót!)
+'''
+nyelvek = []
+
+with open ('Timeline_of_ programming_languages.txt', 'r', encoding="utf-8") as forrasfajl:
+    next(forrasfajl)
+    next(forrasfajl)
+    for sor in forrasfajl:
+        adatok = sor.strip().split(';')
+        nyelv = adatok[1]
+        evszam = adatok[0]
+        nyelvek.append([nyelv, evszam])
+
+for nyelv in nyelvek:
+    print(nyelv)
+
+with open('kimenet.txt', 'w', encoding='utf-8') as celfajl:
+        print('Ez kerül a fájlba...', file=celfajl)  
+        for sor in forrasfajl:
+            print(sor.strip(), file=celfajl)
+  
